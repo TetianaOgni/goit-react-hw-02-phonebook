40a526d3ed810cb80549f8fa74bee16cc2e1e068
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-06-23 13:48:17 +0300

    create App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        {/* <ContactForm /> */}
+
+        <h2>Contacts</h2>
+        {/* <Filter />
+      <ContactList /> */}
+      </div>
+    );
+  }
+}
+export default App;
